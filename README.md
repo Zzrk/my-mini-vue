934cd37eaef464fb5c2aea934f296bf86db83f8c
Zzrk <1252124617@qq.com>
2022-08-27 17:59:27 +0800

    feat: ref

--- README.md
+++ README.md
@@ -11,3 +11,5 @@
 2. 注意一致性，对 get、set 同时进行封装
 3. 注意缓存一些常量，如 get、set 等
 4. 面向对象的形式，可以将一些东西抽象成类，如 ReactiveEffect 等
+5. Object.assign()
+6. Object.is()
---
+++ src/reactivity/ref.ts
@@ -0,0 +1,67 @@
+import { hasChanged, isObject } from "../shared";
+import { isTracking, trackEffects, triggerEffects } from "./effect";
+import { reactive } from "./reactive";
+
+class RefImpl {
+  private _rawValue: any;
+  private _value: any;
+  public dep;
+  public __v_isRef = true;
+  constructor(value) {
+    this._rawValue = value;
+    // value 是对象时需要用 reactive 包裹
+    this._value = convert(value);
+    this.dep = new Set();
+  }
+  
+  get value() {
+    trackRefValue(this);
+    return this._value;
+  }
+
+  set value(newValue) {
+    // 一定先去修改了 value 的值
+    if (hasChanged(this._rawValue, newValue)) {
+      this._rawValue = newValue;
+      this._value = convert(newValue);
+      triggerEffects(this.dep);
+    }
+  }
+}
+
+function convert(value) {
+  return isObject(value) ? reactive(value) : value;
+}
+
+function trackRefValue(ref) {
+  if (isTracking()) {
+    trackEffects(ref.dep);
+  }
+}
+
+export function ref(value) {
+  return new RefImpl(value);
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    }
+  })
+}
