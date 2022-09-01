a745392db76edfdfb2bfbaf6f7963a014b3eb4a3
Zzrk <1252124617@qq.com>
2022-09-01 22:07:20 +0800

    feat: nextTick

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -4,6 +4,7 @@ import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
 import { shouldUpdateComponent } from "./componentUpdateUtils";
 import { createAppAPI } from "./createApp";
+import { queueJobs } from "./scheduler";
 import { Fragment, Text } from "./vnode";
 
 export function createRenderer(options) {
@@ -392,6 +393,10 @@ export function createRenderer(options) {
         const subTree = instance.render.call(proxy);
         instance.subTree = subTree;
         patch(prevSubTree, subTree, container, instance, anchor);
+      }
+    }, {
+      scheduler() {
+        queueJobs(instance.update);
       }
     })
   }
